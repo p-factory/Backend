78f3a0c66935bf5bf7924a7600075d398bc277b7
ijunsoo <dlwnstni74@gmail.com>
2025-02-23 20:41:04 +0900

    🐛 단어 생성 오류 수정
    ㅍ토리 코드

--- src/main/java/com/api/pactory/word/controller/WordController.java
+++ src/main/java/com/api/pactory/word/controller/WordController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/word")
 @RequiredArgsConstructor
 public class WordController {
     private final WordService wordService;
